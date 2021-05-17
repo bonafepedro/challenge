64379f7da16938d7683f8941c4533e45e09361af
Pedro Bonafé <bonafepedro@gmail.com>
2021-05-16 22:31:19 -0300

    corrección ej 7

--- SQL/EJ7.sql
+++ SQL/EJ7.sql
@@ -1,30 +1,18 @@
-WITH movies_bycustomer AS (SELECT title,
-customer_id, DATE(rental_date) date_rental
-FROM customer 
-INNER JOIN rental USING (customer_id)
-INNER JOIN inventory USING (inventory_id)
-INNER JOIN film USING (film_id)
-GROUP BY customer_id, title, date_rental
-ORDER BY date_rental),
-first_movie_percustomer AS(
+WITH first_rental_dates as (
+   SELECT customer_id, min(rental_date) as rental_date 
+    FROM rental
+    GROUP BY customer_id
+),
+first_inventories AS (
+  SELECT inventory_id 
+   FROM rental 
+   JOIN first_rental_dates USING (customer_id, rental_date)
+)
+SELECT title, count(*)
+ FROM first_inventories
+ JOIN inventory USING (inventory_id)
+ JOIN film USING (film_id)
+GROUP BY title
+ORDER BY count(*) DESC
+LIMIT 3;
 
-SELECT 
-    customer_id,
-    FIRST_VALUE(title) 
-    OVER(
-	PARTITION BY customer_id
-        ORDER BY date_rental
-        RANGE BETWEEN 
-            UNBOUNDED PRECEDING AND 
-            UNBOUNDED FOLLOWING
-    ) first_movie_bycustomer
-FROM 
-    movies_bycustomer
-GROUP BY customer_id, title, date_rental)
-    
-SELECT COUNT(a.customer_id), a.first_movie_bycustomer
-FROM first_movie_percustomer a JOIN first_movie_percustomer b USING (customer_id)
-WHERE a.first_movie_bycustomer = b.first_movie_bycustomer
-GROUP BY a.first_movie_bycustomer
-ORDER BY COUNT(a.customer_id) DESC
-LIMIT 3; 
