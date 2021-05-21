4080473d624e8a829bed41ecda055123e4de2118
Pedro Bonafé <bonafepedro@gmail.com>
2021-05-21 11:06:35 -0300

    finisfinished pokemon details

--- Python/pokemon_details.py
+++ Python/pokemon_details.py
@@ -1,6 +1,6 @@
 import requests
 import csv
-import copy
+from tenacity import retry 
 
 """
 First i make a first scraping to take all the names of the pokemon from the API.
@@ -10,6 +10,10 @@ Finally i iterate the names taken in the first scraping
 and conect to the API with the specific url for each name. 
 With the scraping function i take the specific information that i need, 
 and with that i write the csv file. 
+
+
+I installed the tenacity module to use the retry function,
+to control the possible connection errors caused by the exhaustive query to the API.
 """
 
 def first_scrap():
@@ -30,31 +34,38 @@ def first_scrap():
     else:
         return 404
 
+@retry(Exception, total_tries = 10, initial_wait = 0.5)
+def stop_after_10intents ():
+    print ("Stopping after 10 attempts, and connection is failed")
+
 def scraping (name):
     """
     This is the principal scrap function, heare i take the required information.
     """
     args = {'offset' : 0, 'limit' : 1118 }
-    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}/", params= args)
-    if response.status_code == 200:
-        response_json = response.json()
-        poke_data = {
-                    "ID" : "",
-                    "name": "",
-                    "weight": "",
-                    "height": "",
-                    "base_hp" : ''
-                    }
-        
-        poke_data["ID"] = response_json["id"]
-        poke_data["name"] = response_json["name"]
-        poke_data["weight"] = response_json["weight"]
-        poke_data["height"] = response_json["height"]
-        poke_data["base_hp"] = response_json["stats"][0]["base_stat"]
+    try:
 
-        return poke_data
+        response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}/", params= args)
+        if response.status_code == 200:
+            response_json = response.json()
+            poke_data = {
+                        "ID" : "",
+                        "name": "",
+                        "weight": "",
+                        "height": "",
+                        "base_hp" : ''
+                        }
+            
+            poke_data["ID"] = response_json["id"]
+            poke_data["name"] = response_json["name"]
+            poke_data["weight"] = response_json["weight"]
+            poke_data["height"] = response_json["height"]
+            poke_data["base_hp"] = response_json["stats"][0]["base_stat"]
 
-   
+            return poke_data
+    except requests.exceptions.ConnectionError:
+        stop_after_10intents()
+
         
 def execute ():
     """
@@ -65,16 +76,11 @@ def execute ():
     with open('pokemon_details.csv', 'w', encoding='UTF8', newline='') as f:
         writer = csv.DictWriter(f, fieldnames=headers, extrasaction='ignore')
         writer.writeheader()
-        a = 1
-        b = 3
-        while b<=1119:
-            for id in range(a,b):
-                row = scraping(names[id])
-                writer.writerow(row)
-                print(row)
-            a= copy.deepcopy(b)
-            b+=3
-            
+        for id in names:
+            row = scraping(id)
+            writer.writerow(row)
+            print(row)
+        print("finish")            
     f.close()
 
 
